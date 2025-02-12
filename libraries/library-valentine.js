b5a8047a1238891916018ea3df399001c9dcc05e
skt-school <aleksei.ma@sk-thonburi.ac.th>
2025-02-12 15:26:06 +0700

    Add files via upload

---
+++ libraries/library-valentine.js
@@ -0,0 +1,185 @@
+
+var word1 = document.createElement('img');word1.src = './valentine-media/arrow.png';word1.height = 150;
+var word1_2 = document.createElement('img');word1_2.src = './valentine-media/arrow.png';word1_2.height = 150;
+var true1 = 'arrow';
+var false1 = 'error';
+var false1_2 = 'array';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word2 = document.createElement('img');word2.src = './valentine-media/balloons.png';word2.height = 150;
+var word2_2 = document.createElement('img');word2_2.src = './valentine-media/balloons.png';word2_2.height = 150;
+var true2 = 'balloons';
+var false2 = 'balls';
+var false2_2 = 'bubbles';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word3 = document.createElement('img');word3.src = './valentine-media/bouquet.png';word3.height = 150;
+var word3_2 = document.createElement('img');word3_2.src = './valentine-media/bouquet.png';word3_2.height = 150;
+var true3 = 'bouquet';
+var false3 = 'boutique';
+var false3_2 = 'banquet';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word4 = document.createElement('img');word4.src = './valentine-media/bow.png';word4.height = 150;
+var word4_2 = document.createElement('img');word4_2.src = './valentine-media/bow.png';word4_2.height = 150;
+var true4 = "bow";
+var false4 = "baw";
+var false4_2 = 'bowl';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word5 = document.createElement('img');word5.src = './valentine-media/chocolate.png';word5.height = 150;
+var word5_2 = document.createElement('img');word5_2.src = './valentine-media/chocolate.png';word5_2.height = 150;
+var true5 = 'chocolate';
+var false5 = 'caramel';
+var false5_2 = 'cheese';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word6 = document.createElement('img');word6.src = './valentine-media/couple.png';word6.height = 150;
+var word6_2 = document.createElement('img');word6_2.src = './valentine-media/couple.png';word6_2.height = 150;
+var true6 = 'couple';
+var false6 = 'double';
+var false6_2 = 'trouble';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word7 = document.createElement('img');word7.src = './valentine-media/cupid.png';word7.height = 150;
+var word7_2 = document.createElement('img');word7_2.src = './valentine-media/cupid.png';word7_2.height = 150;
+var true7 = 'cupid';
+var false7 = 'eagle';
+var false7_2 = 'demon';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word8 = document.createElement('img');word8.src = './valentine-media/date.png';word8.height = 150;
+var word8_2 = document.createElement('img');word8_2.src = './valentine-media/date.png';word8_2.height = 150;
+var true8 = 'date';
+var false8 = 'time';
+var false8_2 = 'study';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word9 = document.createElement('img');word9.src = './valentine-media/gift.png';word9.height = 150;
+var word9_2 = document.createElement('img');word9_2.src = './valentine-media/gift.png';word9_2.height = 150;
+var true9 = "gift";
+var false9 = 'bouquet';
+var false9_2 = 'balloon';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word10 = document.createElement('img');word10.src = './valentine-media/heart.png';word10.height = 150;
+var word10_2 = document.createElement('img');word10_2.src = './valentine-media/heart.png';word10_2.height = 150;
+var true10 = "heart";
+var false10 = 'brain';
+var false10_2 = 'stomach';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word11 = document.createElement('img');word11.src = './valentine-media/hug.png';word11.height = 150;
+var word11_2 = document.createElement('img');word11_2.src = './valentine-media/hug.png';word11_2.height = 150;
+var true11 = "hug";
+var false11 = "hit";
+var false11_2 = 'kick';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word12 = document.createElement('img');word12.src = './valentine-media/inclusion.png';word12.height = 150;
+var word12_2 = document.createElement('img');word12_2.src = './valentine-media/inclusion.png';word12_2.height = 150;
+var true12 = "inclusion";
+var false12 = "discrimination";
+var false12_2 = 'racism';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word13 = document.createElement('img');word13.src = './valentine-media/kiss.png';word13.height = 150;
+var word13_2 = document.createElement('img');word13_2.src = './valentine-media/kiss.png';word13_2.height = 150;
+var true13 = "kiss";
+var false13 = "slap";
+var false13_2 = 'spank';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word14 = document.createElement('img');word14.src = './valentine-media/perfume.png';word14.height = 150;
+var word14_2 = document.createElement('img');word14_2.src = './valentine-media/perfume.png';word14_2.height = 150;
+var true14 = "perfume";
+var false14 = "beverage";
+var false14_2 = 'potion';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word15 = document.createElement('img');word15.src = './valentine-media/proposal.png';word15.height = 150;
+var word15_2 = document.createElement('img');word15_2.src = './valentine-media/proposal.png';word15_2.height = 150;
+var true15 = "proposal";
+var false15 = "divorce";
+var false15_2 = "marriage";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word16 = document.createElement('img');word16.src = './valentine-media/puppy.png';word16.height = 150;
+var word16_2 = document.createElement('img');word16_2.src = './valentine-media/puppy.png';word16_2.height = 150;
+var true16 = "puppy";
+var false16 = "cat";
+var false16_2 = "kitten";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word17 = document.createElement('img');word17.src = './valentine-media/ring.png';word17.height = 150;
+var word17_2 = document.createElement('img');word17_2.src = './valentine-media/ring.png';word17_2.height = 150;
+var true17 = "ring";
+var false17 = "earring";
+var false17_2 = "necklace";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word18 = document.createElement('img');word18.src = './valentine-media/rose.png';word18.height = 150;
+var word18_2 = document.createElement('img');word18_2.src = './valentine-media/rose.png';word18_2.height = 150;
+var true18 = "rose";
+var false18 = "iris";
+var false18_2 = "orchid";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word19 = document.createElement('img');word19.src = './valentine-media/teddy_bear.png';word19.height = 150;
+var word19_2 = document.createElement('img');word19_2.src = './valentine-media/teddy_bear.png';word19_2.height = 150;
+var true19 = "teddy bear";
+var false19 = "daddy bear";
+var false19_2 = "butter bear";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var word20 = document.createElement('img');word20.src = './valentine-media/valentine_card.png';word20.height = 150;
+var word20_2 = document.createElement('img');word20_2.src = './valentine-media/valentine_card.png';word20_2.height = 150;
+var true20 = "Valentine's Day card";
+var false20 = "Birthday Gift card";
+var false20_2 = "Christmas Eve card";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    var error = "error!!!";
+	var puzzle_sprint_words = 
+	{
+	"0":[
+	{"id":1,"word":word1,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true1,"fake_translation":false1,"fake_translation2":false1_2,"correct":error},
+	{"id":2,"word":word2,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true2,"fake_translation":false2,"fake_translation2":false2_2,"correct":error},
+	{"id":3,"word":word3,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true3,"fake_translation":false3,"fake_translation2":false3_2,"correct":error},
+	{"id":4,"word":word4,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true4,"fake_translation":false4,"fake_translation2":false4_2,"correct":error},
+	{"id":5,"word":word5,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true5,"fake_translation":false5,"fake_translation2":false5_2,"correct":error},
+	{"id":6,"word":word6,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true6,"fake_translation":false6,"fake_translation2":false6_2,"correct":error},
+	{"id":7,"word":word7,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true7,"fake_translation":false7,"fake_translation2":false7_2,"correct":error},
+	{"id":8,"word":word8,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true8,"fake_translation":false8,"fake_translation2":false8_2,"correct":error},
+	{"id":9,"word":word9,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true9,"fake_translation":false9,"fake_translation2":false9_2,"correct":error},
+	{"id":10,"word":word10,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true10,"fake_translation":false10,"fake_translation2":false10_2,"correct":error},
+	{"id":11,"word":word11,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true11,"fake_translation":false11,"fake_translation2":false11_2,"correct":error},
+	{"id":12,"word":word12,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true12,"fake_translation":false12,"fake_translation2":false12_2,"correct":error},
+	{"id":13,"word":word13,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true13,"fake_translation":false13,"fake_translation2":false13_2,"correct":error},
+	{"id":14,"word":word14,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true14,"fake_translation":false14,"fake_translation2":false14_2,"correct":error},
+	{"id":15,"word":word15,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true15,"fake_translation":false15,"fake_translation2":false15_2,"correct":error},
+	{"id":16,"word":word16,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true16,"fake_translation":false16,"fake_translation2":false16_2,"correct":error},
+	{"id":17,"word":word17,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true17,"fake_translation":false17,"fake_translation2":false17_2,"correct":error},
+	{"id":18,"word":word18,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true18,"fake_translation":false18,"fake_translation2":false18_2,"correct":error},
+	{"id":19,"word":word19,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true19,"fake_translation":false19,"fake_translation2":false19_2,"correct":error},
+	{"id":20,"word":word20,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true20,"fake_translation":false20,"fake_translation2":false20_2,"correct":error},
+	
+	],	
+		};
+
+var puzzle_sprint_words2 = 
+	{
+	"0":[
+	{"id":1,"word":word1_2,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true1,"fake_translation":false1,"fake_translation2":false1_2,"correct":error},
+	{"id":2,"word":word2_2,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true2,"fake_translation":false2,"fake_translation2":false2_2,"correct":error},
+	{"id":3,"word":word3_2,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true3,"fake_translation":false3,"fake_translation2":false3_2,"correct":error},
+	{"id":4,"word":word4_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true4,"fake_translation":false4,"fake_translation2":false4_2,"correct":error},
+	{"id":5,"word":word5_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true5,"fake_translation":false5,"fake_translation2":false5_2,"correct":error},
+	{"id":6,"word":word6_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true6,"fake_translation":false6,"fake_translation2":false6_2,"correct":error},
+	{"id":7,"word":word7_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true7,"fake_translation":false7,"fake_translation2":false7_2,"correct":error},
+	{"id":8,"word":word8_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true8,"fake_translation":false8,"fake_translation2":false8_2,"correct":error},
+	{"id":9,"word":word9_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true9,"fake_translation":false9,"fake_translation2":false9_2,"correct":error},
+	{"id":10,"word":word10_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true10,"fake_translation":false10,"fake_translation2":false10_2,"correct":error},
+	{"id":11,"word":word11_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true11,"fake_translation":false11,"fake_translation2":false11_2,"correct":error},
+	{"id":12,"word":word12_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true12,"fake_translation":false12,"fake_translation2":false12_2,"correct":error},
+	{"id":13,"word":word13_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true13,"fake_translation":false13,"fake_translation2":false13_2,"correct":error},
+	{"id":14,"word":word14_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true14,"fake_translation":false14,"fake_translation2":false14_2,"correct":error},
+	{"id":15,"word":word15_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true15,"fake_translation":false15,"fake_translation2":false15_2,"correct":error},
+	{"id":16,"word":word16_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true16,"fake_translation":false16,"fake_translation2":false16_2,"correct":error},
+	{"id":17,"word":word17_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true17,"fake_translation":false17,"fake_translation2":false17_2,"correct":error},
+	{"id":18,"word":word18_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true18,"fake_translation":false18,"fake_translation2":false18_2,"correct":error},
+	{"id":19,"word":word19_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true19,"fake_translation":false19,"fake_translation2":false19_2,"correct":error},
+	{"id":20,"word":word20_2,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true20,"fake_translation":false20,"fake_translation2":false20_2,"correct":error},
+	
+	],	
+		};
+		
+   var in_rating = '1';
+    var adjust_level = '0';
+	
+	
+
+	
